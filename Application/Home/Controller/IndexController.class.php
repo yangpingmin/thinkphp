ad8af9dfb703ad3c677e58fec07e4133bdc33081
yangpingmin <ypm66@126.com>
2019-06-19 17:00:53 +0800

    on_flush()先输出再等待

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,10 +3,10 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        echo date('Y-m-d H:i:s');
-		echo "<br>";
-		sleep(150);
-		echo date('Y-m-d H:i:s');
-		return true;
+    	echo date('Y-m-d h:i:s').'</br>';
+    	ob_flush();
+    	flush();
+    	sleep(150);
+    	echo date('Y-m-d h:i:s');
     }
 }
